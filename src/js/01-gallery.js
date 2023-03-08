c4b51569de75152cf0ab8d9b02cc3f161d08a33b
maciejkudej <maciejkudej765@gmail.com>
2023-03-08 10:38:27 +0100

    Data update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,29 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+// console.log('Basiaddsssd');
+
+const gallery = document.querySelector('.gallery');
+
+createGalleryItems();
+
+function createGalleryItems() {
+  const items = [];
+  for (let i = 0; i < galleryItems.length; i++) {
+    const galleryItem = document.createElement('a');
+    galleryItem.classList.add('gallery__item');
+    galleryItem.href = galleryItems[i].original;
+    galleryItem.appendChild(createGalleryImage(galleryItems[i]));
+    items.push(galleryItem);
+  }
+  gallery.append(...items);
+}
+
+function createGalleryImage(galleryItemFromList) {
+  let img = document.createElement('img');
+  img.classList.add('gallery__image');
+  img.src = galleryItemFromList.preview;
+  img.alt = galleryItemFromList.description;
+  return img;
+}

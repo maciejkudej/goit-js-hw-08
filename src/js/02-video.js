18db047045efd777b50b03528682c2bcb6a0a66e
maciejkudej <maciejkudej765@gmail.com>
2023-03-09 15:44:21 +0100

    Add task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player.getCurrentTime().then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    });
+  }, 1000)
+);

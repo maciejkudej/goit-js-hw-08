5b85d88c9e3834419c509ef307fa38e8c5634149
maciejkudej <maciejkudej765@gmail.com>
2023-03-10 09:00:31 +0100

    Add corect task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,7 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-// console.log(galleryItems);
-// console.log('Basiaddsssd');
+import { galleryItems } from './gallery-items';
 
 const gallery = document.querySelector('.gallery');
 
